c3ebaea26cdf3f2d067131e38c9fab2e3ad3fbab
Martin Vanegas Ospina <141659905+Mvanegas1505@users.noreply.github.com>
2025-05-07 10:24:29 -0500

    Update views.py

--- DjangoProjectBase/movie/views.py
+++ DjangoProjectBase/movie/views.py
@@ -12,7 +12,7 @@ import io
 import urllib, base64
 
 # Cargar la API Key de OpenAI
-load_dotenv('.../openAI.env')
+load_dotenv('api_keys.env')
 client = OpenAI(api_key=os.environ.get('openai_apikey'))
 
 # Función para calcular similitud de coseno
